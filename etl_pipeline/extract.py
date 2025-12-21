0400d3ced3ee99923df241989053bb2e845780f1
khanh-when <khanh.tnn.nguyen@gmail.com>
2025-12-21 12:21:09 -0600

    Transformation Pipeline V1 Implementation
    The current implementation introduces three functions,
    tickerMap, reorderData, and restructureData.
    
    restructureData uses the two other functions to create a
    dictionary that stores the tickers associated with their respective DF data.
    
    This implementation will continue to be developed to account for potential
    NaN data and data integrity.

--- etl_pipeline/extract.py
+++ etl_pipeline/extract.py
@@ -1,20 +1,20 @@
 import yfinance as yf
 import pandas as pd
 
-def extraction(start_date: str, end_date: str, *ticker: str) -> pd.DataFrame:
-    '''Extract historical stock data from Yahoo Finance in a given date range'''
+def extraction(start_date: str, end_date: str, ticker: list[str]) -> pd.DataFrame:
+    '''Extract historical stock data(s) from Yahoo Finance in a given date range'''
 
     def reformat(*dates) -> pd.to_datetime:
         '''parse start/end dates of mixed formats to datetime object'''
         return pd.to_datetime(dates, format='mixed')
     
-    return yf.download(*ticker, *reformat(start_date, end_date), keepna=True)
+    return yf.download(ticker, *reformat(start_date, end_date), keepna=True)
 
 
 def main():
     start = '2025/1/1'
     end = '2025/3/1'
-    data = extraction(start, end, 'AAPL')
+    data = extraction(start, end,['AAPL', 'MSFT'])
 
     print(data.head().to_string())
     print(data.columns)
--- etl_pipeline/transform.py
+++ etl_pipeline/transform.py
@@ -1,13 +1,35 @@
 import pandas as pd
 from extract import extraction
 
-def reorder(data: pd.DataFrame) -> pd.DataFrame:
+def tickerMap(ticker: list[str]) -> dict:
+    '''Create a dictionary template for each ticker'''
+    return {tick:{} for tick in ticker}
+
+def reorderData(data: pd.DataFrame) -> pd.DataFrame:
     '''Reorder DataFrame columns to Open, Low, High, Close, Volume'''
     return data.loc[:, ['Open', 'Low', 'High', 'Close', 'Volume']]
 
-def main():
-    data = reorder(extraction('2025/1/1', '2025/3/1', ['AAPL', 'MSFT']))
+def restructureData(data: pd.DataFrame, ticker):
+    '''Restructure extracted DataFrame into dictionary of DataFrames per ticker'''
+    tickerData = tickerMap(ticker)
+    df = reorderData(data)
 
+    for tick in tickerData:
+        tickerData[tick]['Open'] = df.Open[tick]
+        tickerData[tick]['Low'] = df.Low[tick]
+        tickerData[tick]['High'] = df.High[tick]
+        tickerData[tick]['Close'] = df.Close[tick]
+        tickerData[tick]['Volume'] = df.Volume[tick]
+    
+    return dict(zip(tickerData.keys(), list(map(lambda x: pd.DataFrame(tickerData[x]), tickerData))))
+
+
+
+
+def main():
+    
+    df = extraction('2025/1/1', '2025/3/1', ['AAPL', 'MSFT'])
+    data = restructureData(df, ['AAPL', 'MSFT'])
 
 if __name__ == '__main__':
     main()
