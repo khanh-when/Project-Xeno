1a48ddc4a654bbdf02e2b4a3223986d121b76e17
khanh-when <khanh.tnn.nguyen@gmail.com>
2025-12-22 11:31:33 -0600

    Transformation Layer Implementation V1
    Current implementation presents a minimally sophisticated
    process of cleaning and formatting the data retrieved from the
    extraction layer.
    
    The end result is a dict[str: pd.DataFrame] which will go to the
    Load layer.
    
    Future updates to the transformation layer will check for
    data integrity and potentially other additional functions
    as the project unfolds.
    
    The transformation layer taught me a lot about how
    versatile and flexible data transformation can be. This process
    requires a level of innovation and planning to create data
    structures and transform raw data into something that can be
    usable.
    
    This layer follows a dynamic approach, multiple stocks can be
    extracted and transformed in a single prompt which is more
    sophisticated than seperately prompting each stock for its data.
    
    The dynamic approach proposed many challenges which broaden and
    forced me to critically think through how store all this data
    in a format that practical and eifficient.
    
    Overall, this layer so far is the most challenging and the next phase
    will go into the loading layer.

--- etl_pipeline/transform.py
+++ etl_pipeline/transform.py
@@ -9,7 +9,7 @@ def reorderData(data: pd.DataFrame) -> pd.DataFrame:
     '''Reorder DataFrame columns to Open, Low, High, Close, Volume'''
     return data.loc[:, ['Open', 'Low', 'High', 'Close', 'Volume']]
 
-def restructureData(data: pd.DataFrame, ticker) -> dict[str, pd.DataFrame]:
+def restructureData(data: pd.DataFrame, ticker: list[str]) -> dict[str: pd.DataFrame]:
     '''Restructure extracted DataFrame into dictionary of DataFrames per ticker'''
     tickerData = tickerMap(ticker)
     df = reorderData(data)
@@ -23,10 +23,37 @@ def restructureData(data: pd.DataFrame, ticker) -> dict[str, pd.DataFrame]:
     
     return dict(zip(tickerData.keys(), list(map(lambda x: pd.DataFrame(tickerData[x]), tickerData))))
 
+def cleanData(data: pd.DataFrame, ticker: list[str]):
+    '''clean and format data per ticker'''
+    cleanedData = {}
+    uncleanedData = restructureData(data, ticker)
+
+    for ticker in uncleanedData.keys():
+        uncleanedData[ticker].iloc[[0, 2, 4, 7, 10, 19], 0] = None
+        uncleanedData[ticker].iloc[[0, 2, 4, 7, 10, 19], 1] = None
+        uncleanedData[ticker].iloc[[0, 2, 4, 7, 10, 19], 3] = None
+    
+    for ticker in uncleanedData.keys():
+
+        if (uncleanedData[ticker].isna().sum().sum() > 0):
+            uncleanedData[ticker].bfill(axis=0, inplace=True)
+            uncleanedData[ticker].ffill(axis=0, inplace=True)
+    
+        cleanedData[ticker] = uncleanedData[ticker].loc[:, ['Open', 'Low', 'High', 'Close']].apply(lambda x: round(x, 2), axis=1).join(uncleanedData[ticker]['Volume'])
+        
+    
+    return cleanedData
+    
+def transformation(data: pd.DataFrame, ticker: list[str] ):
+    '''return final cleaned data as dictionary of DataFrames per ticker'''
+    return cleanData(data, ticker)
+        
 def main():
     
-    df = extraction('2025/1/1', '2025/3/1', ['AAPL', 'MSFT'])
-    data = restructureData(df, ['AAPL', 'MSFT'])
+    df = extraction('2025/1/1', '2025/02/1', ['AAPL', 'MSFT'])
+    data = transformation(df, ['AAPL', 'MSFT'])
 
+    print(data['MSFT'])
+    
 if __name__ == '__main__':
     main()
