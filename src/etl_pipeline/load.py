dffb8b484cf1f344723fde8b643d0d6d7493eca5
khanh-when <khanh.tnn.nguyen@gmail.com>
2025-12-24 18:18:43 -0600

    Load Implementation V1 Complete
    The Pipeline successfully inserts batches of
    transformed data in batches of 10 records per execution.
    
    Low batches allow simple debugging since the scale of
    the project is small and so, the next steps include
    
    writing an automation script, and adding support for the
    stocks relational schema.
    
    Some other future improvements include try and exception
    handling from the mariadb server.

--- src/etl_pipeline/load.py
+++ src/etl_pipeline/load.py
@@ -1,11 +1,24 @@
 import pandas as pd
 import mariadb
-# from extract import extraction
-# from transform import transformation
 
-def loader():
-    pass
+def loader(conn: mariadb.connect, data: list):
+    '''load transformed data into MariaDB server in batches of 10 rows per execution'''
+    query = "INSERT INTO stock_prices VALUES (?, ?, ?, ?, ?, ?, ?)"
 
+    try:
+        with conn.cursor() as cursor:
+            for i in range(0, len(data), 10):
+                cursor.executemany(query, data[i:i+10])
+        
+        print(f"Successfully Inserted {len(data)} Values")
+    
+    except mariadb.IntegrityError as e:
+        print(f"Integrity Error: {e}")
+        raise mariadb.IntegrityError
+
+    except mariadb.Error as e:
+        print(f"Error: {e}")
+        raise mariadb.Error
 
 def main():
     pass
--- src/pipeline.py
+++ src/pipeline.py
@@ -1,6 +1,6 @@
 import mariadb
 from etl_pipeline.extract import extraction
-from etl_pipeline.transform import transformation
+from etl_pipeline.transform import transformation, formatData
 from etl_pipeline.load import loader
 
 
@@ -16,13 +16,35 @@ def connection(db_name):
             autocommit = False
         )
     
-    except mariadb.Error as e:
-        print(f"Error: MariaDB Server Connection {e}")
-        raise mariadb.Error
-
+    except mariadb.OperationalError as e:
+        print(f"Operational Error: {e}")
+        raise mariadb.OperationalError
 
 def main():
+    start = '3/1/2025'
+    end = '5/1/2025'
+    tickers = 'AAPL, MSFT'.split(', ')
+
+    df = extraction(start, end, tickers)
+    df2 = formatData(df, tickers)
+
+    for ticker in df2.values():
+        print(ticker.head())
+
+    print(list(df2.keys()))
+
+    data = transformation(df, tickers)
+
+    for i in range(40):
+        print(data[i])
+    
+    print(len(data))
+
     conn = connection('testdb')
+
+    loader(conn, data)
+
+    conn.commit()    
 
 if __name__ == '__main__':
     main()
