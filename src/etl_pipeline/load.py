463979e9e93298d5e7f1e20f06004b13387b9b49
khanh-when <khanh.tnn.nguyen@gmail.com>
2026-01-02 01:06:56 -0600

    load_stock_price function update
    The function introduces a tail recursive approach
    for insertion. Similar to divide and conquerer algorithms.
    
    This approach solidifies a stronger
    idempotent pipeline in the loading phase.
    
    load_stock may be subject to change
    
    Overall, the update to the load_stock_prices is similar to
    merge sort in that the data list continues halving until
    the integrity error is contained and passed.

--- src/etl_pipeline/load.py
+++ src/etl_pipeline/load.py
@@ -22,19 +22,32 @@ def load_stocks(conn, metaData: list) -> None:
     return 
 
 def load_stock_prices(conn: mariadb.connect, data: list) -> None:
-    '''load transformed price data into MariaDB server in batches of 10 rows per execution'''
-    query = "INSERT INTO stock_prices VALUES (?, ?, ?, ?, ?, ?, ?)"
+    '''Tail-recursively loads transformed stock price records into the MariaDB database in fixed-size batches'''
 
+    query = "INSERT INTO stock_prices VALUES (?, ?, ?, ?, ?, ?, ?)"
+    
     try:
         with conn.cursor() as cursor:
-            for i in range(0, len(data), 10):
-                cursor.executemany(query, data[i:i+10])
+            for idx in range(0, len(data), 10):
+                cursor.executemany(query, data[idx:idx+10])
         
         print(f"Successfully Inserted {len(data)} Items")
 
+    #  on integrity error, split the data and retry
     except mariadb.IntegrityError as e:
         print(f"Integrity Error: {e}")
-        raise mariadb.IntegrityError
+
+    # base case: if data length is 1 or less, return
+        if len(data) <= 1:
+            return
+
+        split = len(data) // 2
+
+        # recursively call load_stock_prices on first half
+        load_stock_prices(conn, data[idx:split])
+
+        # recursively call load_stock_prices on second half
+        load_stock_prices(conn, data[split:])
 
     except mariadb.Error as e:
         print(f"Error: {e}")
